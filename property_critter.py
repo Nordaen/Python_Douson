ce107f2f7effe3104a9f85cad22a74b8e7533868
Vlad Kondratev <sykidayteadres@gmail.com>
2016-04-27 13:58:29 +0300

    Update property_critter.py

--- property_critter.py
+++ property_critter.py
@@ -1,6 +1,6 @@
 class Critter(object):
     def __init__(self,name):
-        print("Появилась на свет новая хрень!")
+        print("Появилась на свет новая зверюга!")
         self.__name = name
     @property
     def name(self):
