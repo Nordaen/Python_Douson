58a99c4c606b853c12274f0f7b93f5fe0c99b65e
Vlad Kondratev <sykidayteadres@gmail.com>
2016-04-27 14:01:06 +0300

    Update property_critter.py

--- property_critter.py
+++ property_critter.py
@@ -5,3 +5,21 @@ class Critter(object):
     @property
     def name(self):
         return self.__name
+   @name.setter
+    def name(self,new_name):
+        if new_name=="":
+            print("Имя зверюшки не может быть пустой.")
+        else:
+            self.__name = new_name
+            print("Имя успешно изменено.")
+    def talk(self):
+        print("Привет, меня зовут",self.name)
+crit = Critter("Барбос")
+crit.talk()
+print("\nМоего зверя зовут",end=" ")
+print(crit.name)
+print("\nПробую изменить имя на Горо")
+crit.name = "Горо"
+print("\nМоего зверя зовут",end=" ")
+print(crit.name)
+    
