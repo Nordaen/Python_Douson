568f1bf43e3485272098c0c8d4b6cde48dd8aa3f
Vlad Kondratev <sykidayteadres@gmail.com>
2016-04-29 20:58:36 +0300

    Added some changes

--- blackjack.py
+++ blackjack.py
@@ -95,6 +95,8 @@ class BJ_Dealer(BJ_Hand):
     def flip_first_card(self):
         first_card = self.cards[0]
         first_card.flip()
+
+
 class BJ_Game(object):
     """
     Игра в блекджек
@@ -115,4 +117,55 @@ class BJ_Game(object):
         for player in self.players:
             if not player.is_busted():
                 sp.append(player)
-        1
+        return sp
+
+    def __additional_cards(self, player):
+        while not player.is_busted() and player.is_hitting():
+            self.deck.deal([player])
+            print(player)
+            if player.is_busted():
+                player.bust()
+
+    def play(self):
+        self.deck.deal(self.players +[self.dealer], per_hand = 2)
+        self.dealer.flip_first_card()
+        for player in self.players:
+            print(player)
+        print(self.dealer)
+        for player in self.players:
+            self.__additional_cards(player)
+        self.dealer.flip_first_card()
+        if not self.still_playing:
+            print(self.dealer)
+        else:
+            print(self.dealer)
+            self.__additional_cards(self.dealer)
+            if self.dealer.is_busted():
+                for player in self.still_playing:
+                    player.win()
+            else:
+                for player in self.still_playing:
+                    if player.total > self.dealer.total:
+                        player.win()
+                    elif player.total < self.dealer.total:
+                        player.lose()
+                    else:
+                        player.push()
+        for player in self.player:
+            player.clear()
+        self.dealer.clear()
+    @staticmethod
+    def main():
+        print("\t\tДобро пожаловать за игровой стол Блек-джека!\n")
+        names = []
+        number = games.ask_number("Сколько всего игроков? (1-7): ",low =1, high = 8)
+        for i in range(number):
+            name =input("Введите имя игрока:")
+            names.append(name)
+            print()
+        game = BJ_Game(names)
+        again = None
+        while again !="n":
+            game.play()
+            again = games.ask_yes_no("Хотите сыграть еще раз?")
+            BJ_Game.main()
